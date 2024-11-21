de0d70802f728e2c5b5e498f9e87cb1f62a437c1
SXchew12 <sitxiang12@gmail.com>
2024-11-21 23:54:02 +0800

    test_with_auth_google_v2

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
+//keys.js
+
 if (process.env.NODE._ENV === 'production'){
     //we are in production
     module.exports = require('./prod');
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI:process.env.MONGO_URI ,
+    cookieKey:process.env.COOKIE_KEY 
+    //mongodb://sitxiang12:<db_password>@<hostname>/?ssl=true&replicaSet=atlas-um0wrd-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0
+};
