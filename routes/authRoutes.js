bfacd5510c1622282dc5de165c3f0ad4bf4d7ca4
SXchew12 <sitxiang12@gmail.com>
2024-11-29 08:09:43 +0800

    test edition

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,27 @@
-const passport = require('passport'); // require original passport module not passport.js
+const passport = require('passport');
 
-module.exports = (app) =>{
-    app.get(
-        '/auth/google',
-        passport.authenticate('google', {
-        scope: ['profile', 'email']
-        })
-    );
-    
-    app.get('/auth/google/callback', passport.authenticate('google'));
-
-    app.get('/api/logout', (req,res) => {
-        req.logout();
-        res.send(req.user);
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
     })
+  );
 
-    app.get('/api/current_user', (req,res) => {
-        res.send(req.user); // make sure someone is already went through the oauth flow
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
-    });
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
